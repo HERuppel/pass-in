7f553bcef7ed93bfb651c1035a121818161b1b93
Renato Ruppel <heruppel2@gmail.com>
2024-04-06 15:38:59 -0300

    feat(service): create attendee service

---
+++ src/main/java/rocketseat/com/passin/services/AttendeeService.java
@@ -0,0 +1,40 @@
+package rocketseat.com.passin.services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.domain.attendee.Attendee;
+import rocketseat.com.passin.domain.checkin.CheckIn;
+import rocketseat.com.passin.dto.attendee.AttendeeDetailsDTO;
+import rocketseat.com.passin.dto.attendee.AttendeesResponseDTO;
+import rocketseat.com.passin.repositories.AttendeeRepository;
+import rocketseat.com.passin.repositories.CheckInRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AttendeeService {
+  private AttendeeRepository attendeeRepository;
+  private CheckInRepository checkInRepository;
+
+  public List<Attendee> getAllAttendeesFromEvent(String eventId) {
+    return this.attendeeRepository.findByEventId(eventId);
+  }
+
+  public AttendeesResponseDTO getEventsAttendee(String eventId) {
+    List<Attendee> attendees = this.getAllAttendeesFromEvent(eventId);
+
+    List<AttendeeDetailsDTO> attendeeDetails = attendees.stream().map(attendee -> {
+      Optional<CheckIn> checkIn = this.checkInRepository.findByAttendeeId(attendee.getId());
+
+      LocalDateTime checkedInAt = checkIn.isPresent() ? checkIn.get().getCreatedAt() : null;
+
+      return new AttendeeDetailsDTO(attendee.getId(), attendee.getName(), attendee.getEmail(), attendee.getCreatedAt(), checkedInAt);
+    }).toList();
+
+    return new AttendeesResponseDTO(attendeeDetails);
+  }
+}
