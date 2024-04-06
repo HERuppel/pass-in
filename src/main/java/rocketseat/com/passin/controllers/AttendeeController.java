1709f8cd031bf6aa31eabffabab2e45fb3fa0afe
Renato Ruppel <heruppel2@gmail.com>
2024-04-06 18:17:42 -0300

    feat(controller): add getAttendeeBadge controller method

--- src/main/java/rocketseat/com/passin/controllers/AttendeeController.java
+++ src/main/java/rocketseat/com/passin/controllers/AttendeeController.java
@@ -2,14 +2,25 @@ package rocketseat.com.passin.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import rocketseat.com.passin.services.AttendeeService;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
-    @GetMapping
-    public ResponseEntity<String> getTest() {
-       return ResponseEntity.ok("success");
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO attendeeBadgeResponse = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+
+        return ResponseEntity.ok(attendeeBadgeResponse);
     }
 }
