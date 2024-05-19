051603d992fa838f303bb234034ef63d426fe330
Renato Ruppel <heruppel2@gmail.com>
2024-05-19 18:15:57 -0300

    feat(security): add security filter and configs and jwt token service class

--- src/main/java/rocketseat/com/passin/config/SecurityConfig.java
+++ src/main/java/rocketseat/com/passin/config/SecurityConfig.java
@@ -1,19 +1,26 @@
 package rocketseat.com.passin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
+  @Autowired
+  SecurityFilter securityFilter;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
@@ -23,15 +30,21 @@ public class SecurityConfig {
             })
             .cors(cors -> cors.disable())
             .authorizeHttpRequests(auth -> {
-                auth.requestMatchers("/error/**").permitAll();
-                auth.requestMatchers("/auth/**").permitAll();
+                auth.requestMatchers(HttpMethod.POST, "/auth/signin").permitAll();
+                auth.requestMatchers(HttpMethod.POST, "/auth/signup").permitAll();
                 auth.anyRequest().authenticated();
             })
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
   }
 
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 }
