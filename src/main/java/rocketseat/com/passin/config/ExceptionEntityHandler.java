32e408b8458e7cf73d7b7cf2752a8aaf9ea9c1f8
Renato Ruppel <heruppel2@gmail.com>
2024-05-18 23:52:52 -0300

    refact(config): rework exception handler to deal with new exceptions

--- src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
+++ src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
@@ -6,11 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import rocketseat.com.passin.domain.attendee.exceptions.AttendeeAlreadyRegistered;
-import rocketseat.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
 import rocketseat.com.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
 import rocketseat.com.passin.domain.event.exceptions.EventFullException;
 import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
+import rocketseat.com.passin.domain.user.exceptions.InvalidUserDataException;
+import rocketseat.com.passin.domain.user.exceptions.UserAlreadyExistsException;
+import rocketseat.com.passin.domain.user.exceptions.UserNotFoundException;
 import rocketseat.com.passin.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
@@ -25,18 +26,23 @@ public class ExceptionEntityHandler {
     return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
   }
 
-  @ExceptionHandler(AttendeeNotFoundException.class)
-  public ResponseEntity<HttpStatusCode> handleAttendeeNotFound(AttendeeNotFoundException exception) {
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<HttpStatusCode> handleAttendeeNotFound(UserNotFoundException exception) {
     return ResponseEntity.notFound().build();
   }
 
-  @ExceptionHandler(AttendeeAlreadyRegistered.class)
-  public ResponseEntity<HttpStatusCode> handleAttendeeAlreadyExists(AttendeeAlreadyRegistered exception) {
+  @ExceptionHandler(UserAlreadyExistsException.class)
+  public ResponseEntity<HttpStatusCode> handleUserAlreadyExists(UserAlreadyExistsException exception) {
     return ResponseEntity.status(HttpStatus.CONFLICT).build();
   }
 
   @ExceptionHandler(CheckInAlreadyExistsException.class)
   public ResponseEntity<HttpStatusCode> handleCheckInAlreadyExists(CheckInAlreadyExistsException exception) {
     return ResponseEntity.status(HttpStatus.CONFLICT).build();
+  }
+
+  @ExceptionHandler(InvalidUserDataException.class)
+  public ResponseEntity<ErrorResponseDTO> handleInvalidUserData(InvalidUserDataException exception) {
+    return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
   }
 }
