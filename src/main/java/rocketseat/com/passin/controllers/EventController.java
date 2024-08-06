595bee35411ad83204a9fbfb72185fd06e6a1476
Renato Ruppel <heruppel2@gmail.com>
2024-08-05 22:08:37 -0300

    feat(event): new controller and service for event creation

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -4,10 +4,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.config.ErrorMessages;
+import rocketseat.com.passin.domain.event.exceptions.InvalidEventCreationException;
+import rocketseat.com.passin.domain.role.Role;
+import rocketseat.com.passin.domain.user.exceptions.UserIsNotEventOwnerException;
+import rocketseat.com.passin.dto.event.CreateEventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.EventService;
+import rocketseat.com.passin.services.TokenService;
+import rocketseat.com.passin.services.UserService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 
 @RestController
 @RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
+  @Autowired
+  private final TokenService tokenService;
+  @Autowired
+  private final UserService userService;
+  @Autowired
+  private final EventService eventService;
 
+  @PostMapping
+  public ResponseEntity<EventResponseDTO> create(@RequestHeader("Authorization") String authorizationHeader, @RequestBody CreateEventRequestDTO body) {
+      if (!body.isValid())
+        throw new InvalidEventCreationException(ErrorMessages.INVALID_EVENT_CREATION_DATA);
+
+      String token = authorizationHeader.replace("Bearer ", "");
+
+      Integer userId = this.tokenService.extractUserIdFromToken(token);
+
+      List<Role> roles = this.userService.getUserRoles(userId);
+
+      boolean isEventOwner = roles.stream().noneMatch(role -> "EVENT_OWNER".equals(role.getName()));
+
+      if (isEventOwner) {
+        throw new UserIsNotEventOwnerException(ErrorMessages.USER_IS_NOT_EVENT_OWNER);
+      }
+
+      EventResponseDTO eventResponseDTO = this.eventService.create(body, userId);
+      
+      return ResponseEntity.ok(eventResponseDTO);
+  }
+  
 }
--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -1,11 +1,68 @@
 package rocketseat.com.passin.services;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.config.ErrorMessages;
+import rocketseat.com.passin.domain.address.Address;
+import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.domain.user.User;
+import rocketseat.com.passin.domain.user.exceptions.UserNotFoundException;
+import rocketseat.com.passin.dto.event.CreateEventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.repositories.AddressRepository;
+import rocketseat.com.passin.repositories.EventRepository;
+import rocketseat.com.passin.repositories.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
+  @Autowired
+  private final EventRepository eventRepository;
+  @Autowired
+  private final UserRepository userRepository;
+  @Autowired
+  private final AddressRepository addressRepository;
 
+  public EventResponseDTO create(CreateEventRequestDTO createEventRequest, Integer ownerId) {
+
+    User owner = this.userRepository.findById(ownerId)
+      .orElseThrow(() -> new UserNotFoundException(ErrorMessages.USER_NOT_FOUND));
+
+    Event newEvent = new Event();
+
+    newEvent.setTitle(createEventRequest.title());
+    newEvent.setDetails(createEventRequest.details());
+    newEvent.setMaximumAttendees(createEventRequest.maximumAttendees());
+    newEvent.setStartDate(createEventRequest.startDate());
+    newEvent.setEndDate(createEventRequest.endDate());
+    newEvent.setOwner(owner);
+    newEvent.setCreatedAt(LocalDateTime.now());
+    
+    if (createEventRequest.address().isPresent()) {
+      Address newAddress = new Address();
+      Address address = createEventRequest.address().get();
+
+      newAddress.setCountry(address.getCountry());
+      newAddress.setUf(address.getUf());
+      newAddress.setCity(address.getCity());
+      newAddress.setStreet(address.getStreet());
+      newAddress.setZipcode(address.getZipcode());
+      newAddress.setDistrict(address.getDistrict() != null ? address.getDistrict() : null);
+      newAddress.setComplement(address.getComplement() != null ? address.getComplement() : null);
+
+      this.addressRepository.save(newAddress);
+
+      newEvent.setAddress(newAddress);
+    }
+
+    this.eventRepository.save(newEvent);
+
+    EventResponseDTO eventCreated = new EventResponseDTO(newEvent, 0);
+
+    return eventCreated;
+  }
 }
